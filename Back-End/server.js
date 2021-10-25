5a3ae10ed9df234ac7ecd38ab9539d3005d592c8
RGaetanGranstek <xynfsu@aol.com>
2021-10-25 16:50:31 +0200

    Mise à jour server.js

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,64 +1,69 @@
+// const http = require("http");
+// const app = require("./app");
+
+// //ont dit à l'application sur quel port elle doit fonctionner
+// app.set("port", process.env.PORT || 3000);
+
+// //  Cette fonction reçoit les objets request et response en tant qu'arguments
+// const server = http.createServer(
+//   // (req, res) => {
+//   // // la méthode end de la réponse renvoye une réponse de type string à l'appelant
+//   // res.end("Cool, le serveur fonctionne !");}
+//   app
+// );
+
+// // process.env.PORT => variable environnement (Là ou tourne le serveur si il envoi un port à utiliser sinon par défaut port 3000)
+// server.listen(process.env.PORT || 3000);
+
+// *********
+// *********
+// *********
+// *********
+// *********
+
 const http = require("http");
 const app = require("./app");
 
-//ont dit à l'application sur quel port elle doit fonctionner
-app.set("port", process.env.PORT || 3000);
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
 
-//  Cette fonction reçoit les objets request et response en tant qu'arguments
-const server = http.createServer(
-  // (req, res) => {
-  // // la méthode end de la réponse renvoye une réponse de type string à l'appelant
-  // res.end("Cool, le serveur fonctionne !");}
-  app
-);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
 
-// process.env.PORT => variable environnement (Là ou tourne le serveur si il envoi un port à utiliser sinon par défaut port 3000)
-server.listen(process.env.PORT || 3000);
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port: " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+    default:
+      throw error;
+  }
+};
 
-// *********
-// *********
-// *********
-// *********
-// *********
+const server = http.createServer(app);
 
-// const normalizePort = (val) => {
-//   const port = parseInt(val, 10);
+server.on("error", errorHandler);
+server.on("listening", () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+  console.log("Listening on " + bind);
+});
 
-//   if (isNaN(port)) {
-//     return val;
-//   }
-//   if (port >= 0) {
-//     return port;
-//   }
-//   return false;
-// };
-
-// const errorHandler = (error) => {
-//   if (error.syscall !== "listen") {
-//     throw error;
-//   }
-//   const address = server.address();
-//   const bind =
-//     typeof address === "string" ? "pipe " + address : "port: " + port;
-//   switch (error.code) {
-//     case "EACCES":
-//       console.error(bind + " requires elevated privileges.");
-//       process.exit(1);
-//     case "EADDRINUSE":
-//       console.error(bind + " is already in use.");
-//       process.exit(1);
-//     default:
-//       throw error;
-//   }
-// };
-
-// const server = http.createServer(app);
-
-// server.on("error", errorHandler);
-// server.on("listening", () => {
-//   const address = server.address();
-//   const bind = typeof address === "string" ? "pipe " + address : "port " + port;
-//   console.log("Listening on " + bind);
-// });
-
-// server.listen(port);
+server.listen(port);
